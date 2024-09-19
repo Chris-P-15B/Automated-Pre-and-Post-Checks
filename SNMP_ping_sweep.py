08cedf333157afc976de7a6887e02b84411d2925
Chris Perkins <perkins.chris0@googlemail.com>
2024-09-19 08:56:57 +0100

    Ping sweep updates

--- SNMP_ping_sweep.py
+++ SNMP_ping_sweep.py
@@ -10,8 +10,7 @@ IP address sorting courtesy of https://www.python4networkengineers.com/posts/how
 
 The S in SNMP standing for "Simple" is a lie!
 
-To Do:
-Add IPv6 support.
+Now using "1.3.6.1.2.1.4.32" & "1.3.6.1.2.1.4.34" OIDs to support more vendors, IPv6 & interfaces with multiple IP addresses.
 """
 
 import pkgutil
@@ -25,13 +24,19 @@ import subprocess
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 
-def extract_ip_from_oid(oid):
-    """Given a dotted OID string, this extracts an IPv4 address from the end of it (i.e. the last four decimals)"""
-    return ".".join(oid.split(".")[-4:])
+def extract_ip_from_oid(oid, ipv4=True):
+    """Given a dotted OID string, this extracts an IP address from the end of it"""
+    # IPv4 = the last four decimals, convert to dottted decimal notation
+    if ipv4:
+        return ".".join(oid.split(".")[-4:])
+    # IPv6 = the last 16 decimals & need to convert to correct hex notation
+    else:
+        hex = "".join([f"{int(i):02x}" for i in oid.split(".")[-16:]])
+        return f"{hex[0:4]}:{hex[4:8]}:{hex[8:12]}:{hex[12:16]}:{hex[16:20]}:{hex[20:24]}:{hex[24:28]}:{hex[28:32]}"
 
 
 def extract_mask_from_value(value):
-    """Given a dotted value string, this extracts a subnet mask from the end of it (i.e. decimals)"""
+    """Given a dotted value string, this extracts a subnet mask from the end of it"""
     return int(value.split(".")[-1:][0])
 
 
@@ -86,10 +91,6 @@ def ping_sweep(target_device, community):
         target,
         0,
         25,
-        # Interface index <-> IP address
-        "1.3.6.1.2.1.4.34",
-        # Interface IP <-> subnet mask
-        "1.3.6.1.2.1.4.32",
         # Interface index <-> name (MIB extensions)
         "1.3.6.1.2.1.31.1.1.1.1",
         lookupMib=False,
@@ -106,11 +107,10 @@ def ping_sweep(target_device, community):
         )
         return
 
-    # Extract the data we need from the response
+    # Extract the interface indexes & names we need from the response
     if_index_to_name = {}
-    if_index_to_address = {}
-    if_index_to_subnet_mask = {}
-    if_unicast_addresses = []
+    if_index_to_ipv4_address = {}
+    if_index_to_ipv6_address = {}
     longest = 0
 
     for r in variables:
@@ -125,77 +125,161 @@ def ping_sweep(target_device, community):
 
             # 1-based index <-> interface name
             if oid[0:23] == "1.3.6.1.2.1.31.1.1.1.1.":
-                if_index_to_name[int(oid[oid.rindex(".") + 1 :])] = value
+                if_index = oid[oid.rindex(".") + 1 :]
+                if_index_to_name[if_index] = value
                 longest = max(longest, len(value))
-            # Confirm unicast IP address
-            if oid[0:20] == "1.3.6.1.2.1.4.34.1.4" and value == "1":
-                if_unicast_addresses.append(extract_ip_from_oid(oid))
 
-            # 1-based index <-> interface IPv4 address
-            if oid[0:16] == "1.3.6.1.2.1.4.34" and value[0:16] == "1.3.6.1.2.1.4.32":
-                if value.split(".")[11] == "1":
-                    if extract_ip_from_oid(oid) in if_unicast_addresses:
-                        if_index_to_address[int(value.split(".")[10])] = (
-                            extract_ip_from_oid(oid)
-                        )
-                # IPv4 address <-> subnet mask
-                if value.split(".")[11] == "1":
-                    if_index_to_subnet_mask[int(value.split(".")[10])] = (
-                        extract_mask_from_value(value)
-                    )
+                if if_index_to_ipv4_address.get(if_index, True):
+                    if_index_to_ipv4_address[if_index] = []
+                if if_index_to_ipv6_address.get(if_index, True):
+                    if_index_to_ipv6_address[if_index] = []
+
+    # Send a GETBULK request for the OIDs we want
+    snmp_engine_error, error_status, error_index, variables = command_generator.bulkCmd(
+        authentication,
+        target,
+        0,
+        25,
+        # Interface index <-> IP address
+        "1.3.6.1.2.1.4.34",
+        # Interface IP <-> subnet mask
+        "1.3.6.1.2.1.4.32",
+        lookupMib=False,
+        lexicographicMode=False,
+    )
+
+    if snmp_engine_error:
+        print(f"Error: {target_device} - {snmp_engine_error}")
+        return
+
+    if error_status:
+        print(
+            f"Error: {target_device} - {error_status.prettyPrint()} at {error_index and variables[int(error_index) - 1][0] or '?'}"
+        )
+        return
+
+    # Extract the IP addressing from the response
+    if_unicast_addresses = []
+
+    for r in variables:
+        for name, val in r:
+            oid = name if isinstance(name, str) else name.prettyPrint()
+            value = val.prettyPrint()
+            if (
+                oid == "No more variables left in this MIB View"
+                or value == "No more variables left in this MIB View"
+            ):
+                continue
+
+            # Confirm unicast IP addresses
+            if oid[0:20] == "1.3.6.1.2.1.4.34.1.4" and value == "1":
+                if oid[21] in ("1", "3"):
+                    if_unicast_addresses.append(extract_ip_from_oid(oid, True))
+                if oid[21] in ("2", "4"):
+                    if_unicast_addresses.append(extract_ip_from_oid(oid, False))
+
+            # 1-based index <-> interface IP address
+            if (
+                oid[0:20] == "1.3.6.1.2.1.4.34.1.5"
+                and value[0:16] == "1.3.6.1.2.1.4.32"
+            ):
+                ip_addr = ""
+                # IPv4
+                if oid[21] in ("1", "3"):
+                    if extract_ip_from_oid(oid, True) in if_unicast_addresses:
+                        ip_addr = f"{extract_ip_from_oid(oid, True)}/{extract_mask_from_value(value)}"
+                        if_index_to_ipv4_address[value.split(".")[10]].append(ip_addr)
+                # IPv6
+                if oid[21] in ("2", "4"):
+                    if extract_ip_from_oid(oid, False) in if_unicast_addresses:
+                        ip_addr = f"{extract_ip_from_oid(oid, False)}/{extract_mask_from_value(value)}"
+                        if_index_to_ipv6_address[value.split(".")[10]].append(ip_addr)
 
     if len(if_index_to_name) == 0:
         print(
             f"Error: {target_device} could not get the interface table, dumping raw data instead:"
         )
         print(if_index_to_name)
-        print(if_index_to_address)
-        print(if_index_to_subnet_mask)
+        print(if_index_to_ipv4_address)
+        print(if_index_to_ipv6_address)
         return
 
     ip_addr_list = []
     for i in if_index_to_name:
-        # Skip interfaces without an IP address
-        if i not in if_index_to_address:
-            continue
+        # Multi-threaded ping of valid host IPv4 addresses for the network, ignoring loopback 127.0.0.0/8 addresses
+        ip_addresses = if_index_to_ipv4_address.get(i, [])
+        if ip_addresses:
+            for ip in ip_addresses:
+                ip_and_host_dict = {}
+                if ip[:3] != "127":
+                    workers = []
+                    # Determine list of IPv4 addresses
+                    ip_network = ipaddress.IPv4Network(ip, strict=False)
+                    for host_ip in ip_network.hosts():
+                        worker = threading.Thread(
+                            target=ping_ip, args=(host_ip.exploded, ip_and_host_dict)
+                        )
+                        workers.append(worker)
+                        worker.start()
+                    for worker in workers:
+                        worker.join()
 
-        ip = if_index_to_address[i]
+                    # Display sorted list of IPv4 addresses & hostnames that responded
+                    for ip_addr in sorted(
+                        ip_and_host_dict.keys(),
+                        key=lambda ip_addr: (
+                            int(ip_addr.split(".")[0]),
+                            int(ip_addr.split(".")[1]),
+                            int(ip_addr.split(".")[2]),
+                            int(ip_addr.split(".")[3]),
+                        ),
+                    ):
+                        ip_addr_list.append(
+                            f"{ip_addr} {ip_and_host_dict[str(ip_addr)]}"
+                        )
 
-        if if_index_to_subnet_mask.get(i, None):
-            mask = "/" + str(if_index_to_subnet_mask[i])
-        else:
-            mask = " (unknown subnet mask)"
+        # Multi-threaded ping of valid host IPv6 addresses for the network, ignoring loopback ::1/128
+        # and fe80::/10 link local addresses
+        ip_addresses = if_index_to_ipv6_address.get(i, [])
+        if ip_addresses:
+            for ip in ip_addresses:
+                ip_and_host_dict = {}
+                if ip[:39] != "0000:0000:0000:0000:0000:0000:0000:0001" and ip[
+                    :3
+                ] not in (
+                    "fe8",
+                    "fe9",
+                    "fea",
+                    "feb",
+                ):
+                    workers = []
+                    # Determine list of IPv6 addresses
+                    ip_network = ipaddress.IPv6Network(ip, strict=False)
+                    for host_ip in ip_network.hosts():
+                        worker = threading.Thread(
+                            target=ping_ip, args=(host_ip.exploded, ip_and_host_dict)
+                        )
+                        workers.append(worker)
+                        worker.start()
+                    for worker in workers:
+                        worker.join()
 
-        # Multi-threaded ping of valid host IP addresses for the network, ignoring loopback 127.0.0.0/8 addresses
-        ip_and_host_dict = {}
-        if ip[: int(ip.index("."))] != "127":
-            workers = []
-            # Determine list of IP addresses, including handling /31 subnet masks
-            ip_network = ipaddress.IPv4Network(ip + mask, strict=False)
-            if mask == "/31":
-                ip_list = [ip_network.network_address, ip_network.broadcast_address]
-            else:
-                ip_list = list(ip_network.hosts())
-
-            for host_ip in ip_list:
-                worker = threading.Thread(
-                    target=ping_ip, args=(host_ip.exploded, ip_and_host_dict)
-                )
-                workers.append(worker)
-                worker.start()
-            for worker in workers:
-                worker.join()
-
-            # Store sorted list of IP addresses & hostnames that responded
-            for ip_addr in sorted(
-                ip_and_host_dict.keys(),
-                key=lambda ip_addr: (
-                    int(ip_addr.split(".")[0]),
-                    int(ip_addr.split(".")[1]),
-                    int(ip_addr.split(".")[2]),
-                    int(ip_addr.split(".")[3]),
-                ),
-            ):
-                ip_addr_list.append(f"{ip_addr} {ip_and_host_dict[str(ip_addr)]}")
+                    # Display sorted list of IPv6 addresses & hostnames that responded
+                    for ip_addr in sorted(
+                        ip_and_host_dict.keys(),
+                        key=lambda ip_addr: (
+                            int(ip_addr.split(":")[0], 16),
+                            int(ip_addr.split(":")[1], 16),
+                            int(ip_addr.split(":")[2], 16),
+                            int(ip_addr.split(":")[3], 16),
+                            int(ip_addr.split(":")[4], 16),
+                            int(ip_addr.split(":")[5], 16),
+                            int(ip_addr.split(":")[6], 16),
+                            int(ip_addr.split(":")[7], 16),
+                        ),
+                    ):
+                        ip_addr_list.append(
+                            f"{ip_addr} {ip_and_host_dict[str(ip_addr)]}"
+                        )
 
     return ip_addr_list
